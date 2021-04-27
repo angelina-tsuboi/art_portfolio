525c36050f2e4847b8954b7b7bf1c58f852dccfe
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-27 17:07:15 +0900

    feat: added new three js scripts

--- assets/js/more.js
+++ assets/js/more.js
@@ -1,115 +1,64 @@
-var renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('myCanvas'), antialias: true });
-renderer.setClearColor(0x808080);
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+var scene, camera, renderer;
 
+var WIDTH  = window.innerWidth;
+var HEIGHT = window.innerHeight;
 
-//CREATION OF THE CAMERA
-var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
-camera.position.set(0, 0, 20);
+var SPEED = 0.01;
 
+function init() {
+    scene = new THREE.Scene();
 
-var controls = new THREE.OrbitControls(camera);
-//controls.update() must be called after any manual changes to the camera's transform
-controls.update();
+    initMesh();
+    initCamera();
+    initLights();
+    initRenderer();
 
-
-//CREATION OF THE SCENE
-var scene = new THREE.Scene();
-
-
-//ADDING LIGHTS TO THE SCENE
-//Ambient Light
-var light1 = new THREE.AmbientLight(0xFFFFFF, 0.5);
-scene.add(light1);
-
-
-//PointLight
-var posizioneLuce = [10, 10, 10];
-
-var light2 = new THREE.PointLight(0xFFFFFF, 0.5, 600);
-light2.castShadow = true;
-light2.position.set(posizioneLuce[0], posizioneLuce[1], posizioneLuce[2]);
-scene.add(light2);
-
-light2.shadow.mapSize.width = 512;
-light2.shadow.mapSize.height = 512;
-light2.shadow.camera.near = 0.5;
-light2.shadow.camera.far = 500;
-
-//For the sake of ■■■■ this is just a visualiser
-vedilo = new THREE.SphereBufferGeometry(1, 32, 32);
-var vediMat = new THREE.MeshBasicMaterial({
-    color: 0x000000,
-    //AGGIUNGERE IL MODO WIREFRAME
-});
-var visto = new THREE.Mesh(vedilo, vediMat);
-visto.position.set(posizioneLuce[0], posizioneLuce[1], posizioneLuce[2]);
-scene.add(visto);
-
-
-//CREATION OF MATERIAL
-//Shoe material
-var material = new THREE.MeshPhongMaterial({
-    map: new THREE.TextureLoader().load('textures/diffuse.jpg'),
-    normalMap: new THREE.TextureLoader().load('textures/normals.jpg'),
-});
-
-//Floor material
-var material1 = new THREE.MeshLambertMaterial({
-    color: 0xAFEEEE,
-    side: THREE.DoubleSide
-});
-
-
-//OBJECTS
-//Load the shoe model
-var loader = new THREE.GLTFLoader();
-loader.load('models/meshia.glb', handle_load);
-
-var mesh;
-
-function handle_load(gltf) {
-    mesh = gltf.scene.children[0];
-    mesh.material = material;
-    mesh.material.shading = THREE.SmoothShading;
-    //mesh.castShadow = true;
-    //mesh.receiveShadow = false;
-    scene.add(mesh);
+    document.body.appendChild(renderer.domElement);
 }
 
-//Creation of the circular floor
-circle = new THREE.CircleBufferGeometry(10, 50);
-var cerchio = new THREE.Mesh(circle, material1);
-cerchio.rotation.x = -1.5708;
-cerchio.position.y = -2;
-//cerchio.castShadow = false;
-//cerchio.receiveShadow = true;
-scene.add(cerchio);
+function initCamera() {
+    camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
+    camera.position.set(0, 3.5, 5);
+    camera.lookAt(scene.position);
+}
 
 
-//EVERY FRAME UPDATE
-render();
+function initRenderer() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(WIDTH, HEIGHT);
+}
 
-var delta = 0;
-var prevTime = Date.now();
+function initLights() {
+    var light = new THREE.AmbientLight(0xffffff);
+    scene.add(light);
+}
 
-function render() {
-    delta += 0.1;
-    if (mesh) {
-        mesh.rotation.y += 0.001;
+var mesh = null;
+function initMesh() {
+    var loader = new THREE.JSONLoader();
+    loader.load('./marmelab.json', function(geometry, materials) {
+        mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+        mesh.scale.x = mesh.scale.y = mesh.scale.z = 0.75;
+        mesh.translation = THREE.GeometryUtils.center(geometry);
+        scene.add(mesh);
+    });
+}
+
+function rotateMesh() {
+    if (!mesh) {
+        return;
     }
 
-    renderer.render(scene, camera);
-    controls.update();
+    mesh.rotation.x -= SPEED * 2;
+    mesh.rotation.y -= SPEED;
+    mesh.rotation.z -= SPEED * 3;
+}
+
+function render() {
     requestAnimationFrame(render);
+    rotateMesh();
+    renderer.render(scene, camera);
 }
 
-
-window.addEventListener('resize', onWindowResize, false);
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
+init();
+render();
